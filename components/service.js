798e77c824e83cb326179753cb241c58170d5928
Anubhav Sinha <sinha.anubhav.79@gmail.com>
2021-09-20 17:43:02 +0530

    marquee added

--- components/service.js
+++ components/service.js
@@ -1,5 +1,22 @@
 import React from "react";
-import { Image } from "react-bootstrap";
+import { Image, Row } from "react-bootstrap";
+const century_ply = "/assets/img/century_ply.png";
+const hettich = "/assets/img/hettich.png";
+const asianpaints = "/assets/img/asianpaints.png";
+const bosch = "/assets/img/bosch.png";
+const fabindia = "/assets/img/fabindia.png";
+const gulmohar_lane = "/assets/img/gulmohar_lane.png";
+const jaquar = "/assets/img/jaquar.png";
+const philips = "/assets/img/philips.png";
+const saint_gobain = "/assets/img/saint_gobain.png";
+const siemens = "/assets/img/siemens.png";
+const ebco = "/assets/img/ebco.png";
+const aristo = "/assets/img/aristo.png";
+const centuryLaminates = "/assets/img/century-laminates.png";
+const greenlam = "/assets/img/greenlam.png";
+const merino = "/assets/img/mernino.png";
+const greenply = "/assets/img/greenply.png";
+const hafele = "/assets/img/hafele.svg";
 
 function Service() {
   return (
@@ -111,6 +128,62 @@ function Service() {
           <br />
           <h4 className="fw-bold">Floor and wall tiling</h4>
         </div>
+        <h3 className="text-center py-4 fw-bold">We work with the best</h3>
+        <marquee direction="left">
+          <div className="d-flex align-items-center">
+            <div>
+              <Image src={century_ply} width="200" alt="..." />
+            </div>
+            <div>
+              <Image src={hettich} width="200" alt="..." />
+            </div>
+            <div>
+              <Image src={asianpaints} width="200" alt="..." />
+            </div>
+            <div>
+              <Image src={bosch} width="200" alt="..." />
+            </div>
+            <div>
+              <Image src={fabindia} width="200" alt="..." />
+            </div>
+            <div>
+              <Image src={gulmohar_lane} width="200" alt="..." />
+            </div>
+            <div>
+              <Image src={jaquar} width="200" alt="..." />
+            </div>
+            <div>
+              <Image src={philips} width="200" alt="..." />
+            </div>
+            <div>
+              <Image src={saint_gobain} width="200" alt="..." />
+            </div>
+            <div>
+              <Image src={siemens} width="200" alt="..." />
+            </div>
+            <div>
+              <Image src={hafele} width="200" alt="..." />
+            </div>
+            <div>
+              <Image src={ebco} width="200" alt="..." />
+            </div>
+            <div>
+              <Image src={aristo} width="200" alt="..." />
+            </div>
+            <div>
+              <Image src={centuryLaminates} width="200" alt="..." />
+            </div>
+            <div>
+              <Image src={greenlam} width="200" alt="..." />
+            </div>
+            <div>
+              <Image src={merino} width="200" alt="..." />
+            </div>
+            <div>
+              <Image src={greenply} width="200" alt="..." />
+            </div>
+          </div>
+        </marquee>
       </div>
     </section>
   );
